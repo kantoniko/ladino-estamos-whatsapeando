f05c46835d57b1ec0378bff8774637917b9e03b1
Gabor Szabo <gabor@szabgab.com>
2022-04-19 09:08:35 +0300

    check for duplicate publication date

--- ladino/whatsapeando.py
+++ ladino/whatsapeando.py
@@ -10,6 +10,7 @@ def get_messages():
     #print(yaml_files)
     ogg_files = set(os.listdir(os.path.join(root, 'sound')))
     #print(ogg_files)
+    pubs = set()
     for yaml_filename in yaml_files:
         with open(os.path.join(root, 'text', yaml_filename)) as fh:
             data = safe_load(fh)
@@ -24,6 +25,11 @@ def get_messages():
         entries.append(data)
         assert re.search(r'^\d\d\d\d\.\d\d\.\d\d$', data['data'], re.ASCII)
         assert re.search(r'^\d\d\d\d\.\d\d\.\d\d$', data['pub'], re.ASCII)
+
+        if data['pub'] in pubs:
+            raise Exception(f"Duplicate publication date {data['pub']}")
+        pubs.add(data['pub'])
+
         assert len(data['titulo']) > 5
     if ogg_files:
         raise Exception(f"Some sound files {ogg_files} are not in use")
