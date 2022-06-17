9def0ad8c1c8b2a620abdced1c062cec0898c44b
Gabor Szabo <gabor@szabgab.com>
2022-06-17 17:01:05 +0300

    print latest title

--- ladino/whatsapeando.py
+++ ladino/whatsapeando.py
@@ -77,6 +77,6 @@ if __name__ == '__main__':
     entries = get_messages()
     #print(entries)
     print(f"All read properly.")
-    print(f"Last published:   {entries[-1]['pub']}")
+    print(f"Last published:   {entries[-1]['pub']} - {entries[-1]['titulo']}")
     print(f"Today is          {str(datetime.date.today()).replace('-', '.')}")
 
